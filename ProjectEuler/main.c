ae74c0095555997dbbadd212358c7f42dbf9ef8f
MonsieurSingh <66196010+MonsieurSingh@users.noreply.github.com>
2023-12-19 13:21:24 +1100

    Initial Commit

---
+++ ProjectEuler/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ProjectEuler
+//
+//  Created by Devjyot Singh on 19/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
